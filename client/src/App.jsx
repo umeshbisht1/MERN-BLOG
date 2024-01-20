20f9b09da0274a3226381520f5bb03453594a117
umesh bisht1 <umeshbisht792@gmail.com>
2024-01-20 16:14:40 +0530

    install react js and tailwind css

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='bg-red-900'>umesh singh bisht</h1>
+   <h3>umesh singh</h3>   
+    </div>
+  )
+}
+
+export default App
