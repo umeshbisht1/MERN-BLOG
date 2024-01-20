3263a2d46ce30707ea6133745826af60b0eb994e
umesh bisht1 <umeshbisht792@gmail.com>
2024-01-20 16:29:59 +0530

    created the routes and install the files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import About from './pages/About.jsx'
+import Signin from './pages/Signin.jsx'
+import Signup from './pages/Signup.jsx'
+import Project from './pages/Project.jsx'
+import Dashboard from './pages/Dashboard.jsx'
+import Home from './pages/Home.jsx'
 function App() {
   return (
-    <div>
-      <h1 className='bg-red-900'>umesh singh bisht</h1>
-   <h3>umesh singh</h3>   
-    </div>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<Home />}></Route>
+    <Route path='/about' element={<About />}></Route>
+    <Route path='/signin' element={<Signin />}></Route>
+    <Route path='/signup' element={<Signup />}></Route>
+    <Route path='/dashborad' element={<Dashboard/>}></Route>
+    <Route path='/project' element={<Project />}></Route>
+   </Routes>
+   
+   </BrowserRouter>
   )
 }
 
