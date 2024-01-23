fbbce6de1e668f5e6113beed206ef7f6d89a858d
umesh bisht1 <umeshbisht792@gmail.com>
2024-01-23 19:42:12 +0530

    dark mode modification

--- client/src/components/ThemeProvider.jsx
+++ client/src/components/ThemeProvider.jsx
@@ -5,7 +5,8 @@ function ThemeProvider({children}) {
    
   return (
     <div className={theme}>
-     <div className=" bg-white text-grey-700 dark:text-grey-200 dark:bg-[rgb(16,23,42)] min-h-screen">
+     <div className=" bg-white  text-grey-200  dark:text-white
+      dark:bg-[rgb(16,23,42)] min-h-screen">
           {children}
      </div>
     </div>
